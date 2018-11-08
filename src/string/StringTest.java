acfaf1f6bc2ddd6f742c29ed87f8995d1aca2827
jqorz <jia13084056886>
2018-11-08 14:34:00 +0800

    字母数组

--- src/string/StringTest.java
+++ src/string/StringTest.java
@@ -1,6 +1,8 @@
 package string;
 
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author jqorz
@@ -27,7 +29,7 @@ class StringTest {
         return okLen;
     }
 
-    public static void main(String[] args) {
+    public static void charset() {
         String s = "我爱你";
         try {
             String s1 = new String(s.getBytes("utf-8"), "gb2312");
@@ -41,5 +43,38 @@ class StringTest {
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * 把26个字母4个一组，竖着排
+     */
+    public static void charAll() {
+        String[] source = new String[28];
+        char c = 'a';
+        for (int i = 0; i < 28; i++) {
+            if (i == 20) {
+                source[i] = "0";
+                continue;
+            }
+            if (i == 27) {
+                source[i] = "0";
+                continue;
+            }
+            source[i] = c + "";
+            c++;
+        }
+        List<String> data = new ArrayList<>();
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (i + 7 * j < 28) {
+                    data.add(source[i + 7 * j]);
+                }
+            }
+        }
+        System.out.println(data);
+    }
+
+    public static void main(String[] args) {
+        charAll();
     }
 }
