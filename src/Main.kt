bc81dac6cd3d42ad796f3d687e218e5018f01e26
qijia <j1997321@qq.com>
2021-03-17 09:50:13 +0800

    新增kotlin和gson

---
+++ src/Main.kt
@@ -0,0 +1,23 @@
+import utils.GsonUtil
+
+object Main {
+
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+
+        val models = mutableListOf(Fruit("aa1", "bb1"), Fruit("aa2", "bb2"))
+        val maps = HashMap<String, String>()
+        models.forEach {
+            maps[it.aa] = it.bb
+        }
+        println(GsonUtil.jsonCreate(models))
+        println(GsonUtil.jsonCreate(maps))
+
+    }
+
+    class Fruit(val aa: String, val bb: String)
+}
+
+
+
