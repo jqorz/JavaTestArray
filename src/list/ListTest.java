f248ac9e58b97e64fd3c98fa40549a913a76f45e
jqorz <j1997321@qq.com>
2022-04-28 21:34:27 +0800

    list test

---
+++ src/list/ListTest.java
@@ -0,0 +1,20 @@
+package list;
+
+import utils.GsonUtil;
+
+import java.util.ArrayList;
+
+public class ListTest {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            list.add(String.valueOf(i));
+        }
+        for (int i = 0; i < list.size(); i++) {
+            if (i==3){
+                list.add(3,"s");
+            }
+        }
+        System.out.println(GsonUtil.jsonCreate(list));
+    }
+}
