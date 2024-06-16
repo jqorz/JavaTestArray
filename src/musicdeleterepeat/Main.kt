259889a2d215518a3b7099e5932616f3df4b2bbe
jqorz <j1997321@qq.com>
2024-06-16 11:50:39 +0800

    新增从mp3中删除已有的ncm

--- src/musicdeleterepeat/Main.kt
+++ src/musicdeleterepeat/Main.kt
@@ -9,15 +9,29 @@ object Main {
 
     @JvmStatic
     fun main(args: Array<String>) {
+        deleteMultiInFolder()
+//        deleteRepeat()
+    }
+
+
+    private fun deleteMultiInFolder() {
         val dir = File("D:\\我的音乐")
         val children = dir.listFiles()
         val deleteFiles = ArrayList<File>()
-        val processFileList = children?.filter { it.isFile && it.name.toLowerCase().endsWith("mp3") }
+        children?.forEach {
+            if (it.name.endsWith(".MP3")) {
+                it.renameTo(File(it.name.replace(".MP3", ".mp3")))
+            }
+        }
+        val processFileList = children?.filter { it.isFile && it.name.endsWith("mp3") }
         processFileList!!.groupBy {
-            it.name.replace(Regex(" \\(\\d+\\)"), "").replace(" ", "").replace(",","").toUpperCase().trim()
+            val groupKey =
+                it.nameWithoutExtension.replace(Regex("\\(\\d+\\)"), "").replace(" ", "").replace(",", "").toUpperCase()
+                    .trim()
+            groupKey
         }.forEach { (_, fileList) ->
             val newestOne = fileList.maxBy { it.lastModified() }!!
-            val wantName = newestOne.name.replace(Regex(" \\(\\d+\\)"), "").trim()
+            val wantName = newestOne.name.replace(Regex("\\(\\d+\\)"), "").trim()
             if (fileList.size > 1) {
                 val delete = fileList - newestOne
                 deleteFiles.addAll(delete)
@@ -41,6 +55,36 @@ object Main {
         }
     }
 
+
+    private fun deleteRepeat() {
+        val dir1 = File("D:\\我的音乐")
+        val dir2 = File("D:\\我的音乐2")
+        val mp3Files = dir1.listFiles() ?: return
+        val ncmFiles = dir2.listFiles() ?: return
+        val wantDeleteList = arrayListOf<File>()
+
+        ncmFiles.forEach { file ->
+            var contain = false
+            mp3Files.forEach {
+                if (it.nameWithoutExtension.toLowerCase().replace(",", " ") == file.nameWithoutExtension.toLowerCase()
+                    || it.nameWithoutExtension.toLowerCase().replace(Regex("(\\S+),(.*)-"), "\$1 \$2")
+                    == file.nameWithoutExtension.toLowerCase()
+                ) {
+                    wantDeleteList.add(it)
+                    contain = true
+                }
+            }
+            if (!contain) {
+                println("Can not checked file is ${file.nameWithoutExtension}")
+            }
+        }
+        println("================================================")
+        println("Sum want delete count is ${wantDeleteList.size} , dir2's file count is ${ncmFiles.size}")
+        wantDeleteList.forEach {
+            it.deleteOnExit()
+        }
+        println("Delete finished")
+    }
 }
 
 
