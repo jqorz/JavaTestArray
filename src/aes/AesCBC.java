69951309ba818cfb9168969395cef5e64c6a71b4
jqorz <j1997321@qq.com>
2024-06-16 10:31:05 +0800

    支持jdk17

--- src/aes/AesCBC.java
+++ src/aes/AesCBC.java
@@ -8,9 +8,8 @@ package aes;
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
 
-import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
 
 /**
  * AES 是一种可逆加密算法，对用户的敏感信息加密处理
@@ -19,21 +18,24 @@ import sun.misc.BASE64Encoder;
  */
 public class AesCBC {
     /*已确认
-    * 加密用的Key 可以用26个字母和数字组成
-    * 此处使用AES-128-CBC加密模式，key需要为16位。
-    */
-    private static String sKey="asdfwetyhjuytrfd";
-    private static String ivParameter="gfdertfghjkuyrtg";
-    private static AesCBC instance=null;
+     * 加密用的Key 可以用26个字母和数字组成
+     * 此处使用AES-128-CBC加密模式，key需要为16位。
+     */
+    private static String sKey = "asdfwetyhjuytrfd";
+    private static String ivParameter = "gfdertfghjkuyrtg";
+    private static AesCBC instance = null;
+
     //private static
-    private AesCBC(){
+    private AesCBC() {
 
     }
-    public static AesCBC getInstance(){
-        if (instance==null)
-            instance= new AesCBC();
+
+    public static AesCBC getInstance() {
+        if (instance == null)
+            instance = new AesCBC();
         return instance;
     }
+
     // 加密
     public String encrypt(String sSrc, String encodingFormat, String sKey, String ivParameter) throws Exception {
         Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
@@ -42,7 +44,7 @@ public class AesCBC {
         IvParameterSpec iv = new IvParameterSpec(ivParameter.getBytes());//使用CBC模式，需要一个向量iv，可增加加密算法的强度
         cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
         byte[] encrypted = cipher.doFinal(sSrc.getBytes(encodingFormat));
-        return new BASE64Encoder().encode(encrypted);//此处使用BASE64做转码。
+        return Base64.getEncoder().encodeToString(encrypted);//此处使用BASE64做转码。
     }
 
     // 解密
@@ -53,9 +55,9 @@ public class AesCBC {
             Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
             IvParameterSpec iv = new IvParameterSpec(ivParameter.getBytes());
             cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
-            byte[] encrypted1 = new BASE64Decoder().decodeBuffer(sSrc);//先用base64解密
+            byte[] encrypted1 = Base64.getDecoder().decode(sSrc);//先用base64解密
             byte[] original = cipher.doFinal(encrypted1);
-            String originalString = new String(original,encodingFormat);
+            String originalString = new String(original, encodingFormat);
             return originalString;
         } catch (Exception ex) {
             return null;
@@ -65,15 +67,15 @@ public class AesCBC {
     public static void main(String[] args) throws Exception {
         // 需要加密的字串
         String cSrc = "111111";
-        System.out.println("加密前的字串是："+cSrc);
+        System.out.println("加密前的字串是：" + cSrc);
         // 加密
-        String enString = AesCBC.getInstance().encrypt(cSrc,"utf-8",sKey,ivParameter);
-        System.out.println("加密后的字串是："+ enString);
+        String enString = AesCBC.getInstance().encrypt(cSrc, "utf-8", sKey, ivParameter);
+        System.out.println("加密后的字串是：" + enString);
 
         System.out.println("1jdzWuniG6UMtoa3T6uNLA==".equals(enString));
 
         // 解密
-        String DeString = AesCBC.getInstance().decrypt(enString,"utf-8",sKey,ivParameter);
+        String DeString = AesCBC.getInstance().decrypt(enString, "utf-8", sKey, ivParameter);
         System.out.println("解密后的字串是：" + DeString);
     }
 }
